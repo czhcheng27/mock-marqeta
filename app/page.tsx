438987a578639b88dcf7c7b1c2f68142f212742a
zihang cheng <zihang.cheng@ott.ca>
2026-03-16 10:09:32 -0400

    add HeroCard, OverflowWrapper to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import HeroCard from "@/components/HeroCard";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+import OverflowWrapper from "@/components/OverflowWrapper";
+import ResultsSection from "@/components/ResultsSection";
 
 export default function Home() {
   return (
     <main id="main" className="">
       <div className="">
         <HeroSection />
+        <HeroCard />
+        <OverflowWrapper />
+        {/* <ResultsSection /> */}
+        {/* <div className="h-3000 bg-[black]"></div> */}
       </div>
     </main>
   );
